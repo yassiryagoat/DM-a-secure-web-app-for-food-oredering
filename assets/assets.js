12bc7fad39a35e849f1d332f8c931e96a4eb2f06
YAGOAT <yassiryagout@gmail.com>
2025-10-03 17:03:14 +0100

    Add assets folder

---
+++ assets/assets.js
@@ -0,0 +1,14 @@
+import add_icon from './add_icon.png'
+import order_icon from './order_icon.jpeg'
+import parcel_icon from './parcel_icon.jpg'
+import profile_image from './profie_image2.jpg'
+import upload_image from './upload_image3.jpeg'
+
+
+export const assets = {
+    add_icon,
+    order_icon,
+    parcel_icon,
+    profile_image,
+    upload_image
+}
