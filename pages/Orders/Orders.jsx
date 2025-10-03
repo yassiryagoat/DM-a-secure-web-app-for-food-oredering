4780bd1f303a31f19a10c24f7f7c349f9a3cce91
YAGOAT <yassiryagout@gmail.com>
2025-10-03 17:08:05 +0100

    Add compo,pages and server folders

---
+++ pages/Orders/Orders.jsx
@@ -0,0 +1,11 @@
+import react from 'react'
+import './Orders.css'
+import { assets } from '../../assets/assets';
+const Orders=()=>{
+    return(
+        <div className='Orders'>
+            Orders
+        </div>
+    )
+}
+export default Orders;
